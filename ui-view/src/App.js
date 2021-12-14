1930bddedc26b65045db9eea9aa1bb3b12bec7e4
duydoit <duy.da150597@sis.hust.edu.vn>
2021-12-14 21:36:17 +0700

    view

--- ui-view/src/App.js
+++ ui-view/src/App.js
@@ -1,14 +1,9 @@
 import Navbar from "./components/navbar/Navbar";
-import Employee from "./components/router/employee/Employee";
-
 
 function App() {
   return (
-    <div className="App">
-        <Navbar/>
-        <div className="mt-15">
-          <Employee/>
-        </div>
+    <div>
+      <Navbar />
     </div>
   );
 }
